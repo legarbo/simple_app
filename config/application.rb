188f8a8c04b9803dbcdf68596e76e29d5f3bed2c
Leopoldo Garcia <legarbo@gmail.com>
2011-11-08 21:24:53 -0430

    Update Gemfile

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,12 @@ require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
-if defined?(Bundler)
+#if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
+  #Bundler.require *Rails.groups(:assets => %w(development test))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
-end
+#end
 
 module SimpleApp
   class Application < Rails::Application
@@ -46,7 +46,7 @@ module SimpleApp
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    #config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
